b3735eb33d818068ce1544ff5294dc0817a8a7e9
Sushil <sushilkumar811544@gmail.com>
2024-05-22 10:45:31 +0530

    Find Jobs

--- Frontend/src/components/index.js
+++ Frontend/src/components/index.js
@@ -3,8 +3,8 @@ import Footer from "./Footer";
 import CustomButton from "./CustomButton";
 import TextInput from "./TextInput";
 import SignUp from "./SignUp";
-// import Header from "./Header";
-// import ListBox from "./ListBox";
+import Header from "./Header";
+import ListBox from "./ListBox";
 // import JobCard from "./JobCard";
 // import Loading from "./Loading";
 // import CompanyCard from "./CompanyCard";
@@ -16,8 +16,8 @@ export {
   CustomButton,
   TextInput,
   SignUp,
-//   Header,
-//   ListBox,
+  Header,
+  ListBox,
 //   JobCard,
 //   Loading,
 //   CompanyCard,
